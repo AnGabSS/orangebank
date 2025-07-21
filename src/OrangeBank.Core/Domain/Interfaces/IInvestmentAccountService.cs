553bd31d282484e821ff249237cc3b71b3b1b787
AnGabSS <angelo.gs.santana@outlook.com>
2025-07-21 15:02:07 -0300

    [FEATURE] ADD interface for service and repository for Investment Account entity

---
+++ src/OrangeBank.Core/Domain/Interfaces/IInvestmentAccountService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrangeBank.Core.Domain.Entities;
+
+namespace OrangeBank.Core.Domain.Interfaces
+{
+    public interface IInvestmentAccountService
+    {
+        Task<InvestmentAccount> RegisterAsync(Guid userId);
+        Task<InvestmentAccount?> GetByIdAsync(Guid accountId);
+        Task<InvestmentAccount?> GetByUserIdAsync(Guid userId);
+        Task<InvestmentAccount?> GetByAccountNumberAsync(string accountNumber);
+        Task<InvestmentAccount> Invest(string accountNumber, Asset asset, int quantity);
+        Task<InvestmentAccount> WithDraw(string accountNumber, Asset asset, int quantity);
+    }
+}
