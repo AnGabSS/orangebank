e162e4fdd506a53f4a4786259e9626a3b74fb48b
AnGabSS <angelo.gs.santana@outlook.com>
2025-08-01 08:33:43 -0300

    [FEATURE] Initial Transaction Service

---
+++ src/OrangeBank.Application/Services/TransactionService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrangeBank.Core.Domain.Entities;
+using OrangeBank.Core.Domain.Interfaces;
+
+namespace OrangeBank.Application.Services;
+
+public class TransactionService : ITransactionService
+{
+
+    private ITransactionRepository _repository;
+
+    public TransactionService(ITransactionRepository transactionRepository)
+    {
+        _repository = transactionRepository;
+    }
+
+    public async Task AddAsync(Transaction transaction)
+    {
+        await _repository.AddAsync(transaction);
+    }
+
+    public async Task<IEnumerable<Transaction>> GetAllAsync()
+    {
+        return await _repository.GetAllAsync();
+    }
+
+    public async Task<IEnumerable<Transaction>> GetByAccountAsync(string accountNumber)
+    {
+        return await _repository.GetByAccountAsync(accountNumber);
+    }
+
+    public async Task<IEnumerable<Transaction>> GetByDestinyAccountAsync(string destinyAccount)
+    {
+        return await _repository.GetByDestinyAccountAsync(destinyAccount);
+    }
+
+    public async Task<Transaction?> GetByIdAsync(Guid transactionId)
+    {
+        return await _repository.GetByIdAsync(transactionId);
+    }
+
+    public async Task<IEnumerable<Transaction>> GetByOriginAccountAsync(string originAccount)
+    {
+        return await _repository.GetByOriginAccountAsync(originAccount);
+    }
+
+    public async Task<IEnumerable<Transaction>> GetByUserAsync(Guid userId)
+    {
+        return await _repository.GetByUserAsync(userId);
+    }
+}
+
--- src/OrangeBank.Core/Domain/Entities/Transaction.cs
+++ src/OrangeBank.Core/Domain/Entities/Transaction.cs
@@ -11,13 +11,13 @@ namespace OrangeBank.Core.Domain.Entities
     {
 
         public Guid Id { get; protected set; }
-        public string OriginAccount { get; protected set; }
-        public string DestinyAccount { get; protected set; }
+        public Account OriginAccount { get; protected set; }
+        public Account DestinyAccount { get; protected set; }
         public decimal Amount { get; protected set; }
         public TransactionType type { get; protected set; }
         public DateTime CreatedAt { get; protected set; }
 
-        public Transaction(string originAccount, string destinyAccount, decimal amount, TransactionType type)
+        public Transaction(Account originAccount, Account destinyAccount, decimal amount, TransactionType type)
         {
             Id = Guid.NewGuid();
             OriginAccount = originAccount ?? throw new ArgumentNullException(nameof(originAccount));
